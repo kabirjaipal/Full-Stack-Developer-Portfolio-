6d004d98b82c7141674955380893a11fa52d1f61
Fire Bird <73753258+kabirsingh2004@users.noreply.github.com>
2023-02-20 16:14:20 +0530

    updated footer with credit ✅
    updated footer with credit ✅

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800">
       <div className="max-w-6xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <p className="text-center text-gray-400">
-          &copy; 2023 Full Stack Developer. All rights reserved.
+          &copy; Coded By Kabir Jaipal. All rights reserved.
         </p>
       </div>
     </footer>
